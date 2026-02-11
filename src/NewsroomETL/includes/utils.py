6a56e4720225fa330f2b1daa59160e951c01a4a0
Andres <aforjuela11@misena.edu.co>
2026-02-11 11:40:36 -0500

    chore(logging): improve log format

--- src/NewsroomETL/includes/utils.py
+++ src/NewsroomETL/includes/utils.py
@@ -5,7 +5,10 @@ from pyspark.sql import SparkSession
 import logging
 import yaml
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
 logger = logging.getLogger(__name__)
 
 
