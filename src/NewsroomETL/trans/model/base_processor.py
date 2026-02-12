c2ecbc4e7ac60677070f3e7270611061cfb1dcd3
Andres <aforjuela11@misena.edu.co>
2026-02-12 10:57:39 -0500

    refactor(processor): decouple Spark session from BaseProcessor and introduce explicit bucket paths

--- src/NewsroomETL/trans/model/base_processor.py
+++ src/NewsroomETL/trans/model/base_processor.py
@@ -7,16 +7,17 @@ config = get_env_conf()
 
 
 class BaseProcessor(object):
-    def __init__(self,source) -> None:
-        self.bucket_output = config["s3"]["bucket_zones"]["silver_zone"]
-        self.bucket_input = config["s3"]["bucket_zones"]["bronze_zone"]
+    def __init__(self) -> None:
+        self.bucket_bronze = config["s3"]["bucket_zones"]["bronze_zone"]
+        self.bucket_silver = config["s3"]["bucket_zones"]["silver_zone"]
+        self.bucket_gold = config["s3"]["bucket_zones"]["gold_zone"]
         self.environment = config["environment"]
-        self.spark_utils = SparkUtils()
-        self.source = source
-        self.spark = self.spark_utils.get_spark_session(app_name=f"DataTransformation")
 
-    def get_bronze_path(self, section, ingestion_date) -> str:
-        return f"{self.bucket_input}/{self.environment}/{self.source}/ingestion_date={ingestion_date}/section={section}/"
+    def get_bronze_path(self, section, ingestion_date, source) -> str:
+        return f"{self.bucket_bronze}/{self.environment}/{source}/ingestion_date={ingestion_date}/section={section}/"
 
     def get_silver_path(self, table_name) -> str:
-        return f"{self.bucket_output}/{self.environment}/{table_name}"
+        return f"{self.bucket_silver}/{self.environment}/{table_name}"
+
+    def get_gold_path(self, table_name) -> str:
+        return f"{self.bucket_gold}/{self.environment}/{table_name}"
